d73b47a7462dd70b502ee5b7942ce633416d7bf2
Pierpaolo Calanna <p.calanna@gmail.com>
2025-04-16 08:02:59 +0200

    update TUI app

--- lib/textual/screens/file_screen.py
+++ lib/textual/screens/file_screen.py
@@ -1,17 +1,17 @@
 from pathlib import Path
 from typing import Iterable
-from textual import on
 from textual.reactive import reactive
 from textual.app import ComposeResult
 from textual.screen import Screen
 from textual.widgets import Label, DirectoryTree, Static
-
-class CSVTree(DirectoryTree):
-    def filter_paths(self, paths: Iterable[Path]) -> Iterable[Path]:
-        return [path for path in paths if path.suffix.__eq__(".csv")]
+from textual.containers import HorizontalGroup
 
 class FileScreen(Screen):
 
+    class CSVTree(DirectoryTree):
+        def filter_paths(self, paths: Iterable[Path]) -> Iterable[Path]:
+            return [path for path in paths if path.suffix.__eq__(".csv") or path.is_dir()]
+        
     CSS = """
 
     FileScreen > * {
@@ -21,29 +21,37 @@ class FileScreen(Screen):
     CSVTree {
         height: auto;
         padding: 2;
-        background: #000;
+        background: rgb(18,18,18);
     }
 
     SelectedFile {
         height: 1;
     }
 
+    HorizontalGroup > Label  {
+        margin-right: 1
+    }
+
 """
+    selected_file = reactive("", layout=True)
 
     def __repr__(self) -> str:
         return "fileScreen"
 
-    selected_file = reactive("", layout=True)
+    def on_mount(self) -> None:
+        self.CSVTree.show_root = False # type: ignore
+        
     
-    def watch_selected_file(self, newValue: str):
-        self.query_one("#selected_file", Static).update(newValue)
+    def watch_selected_file(self, selected_file: str):
+        self.query_one("#selected_file", Static).update(selected_file)
     
-    @on(CSVTree.FileSelected)
-    def on_file_selected(self, event: CSVTree.FileSelected) -> None:
+    def on_directory_tree_file_selected(self, event: CSVTree.FileSelected) -> None:
+        print(self.CSVTree.FileSelected.handler_name)
         self.selected_file = event.path.name
 
     def compose(self) -> ComposeResult:
         yield Label("Scegli il file contenente i dati dei test da siglare e premi <invio>")
-        yield CSVTree("./data")
-        yield Label("File selezionato:")
-        yield Static(id="selected_file")
+        with HorizontalGroup():
+            yield Label("File selezionato:")
+            yield Static(id="selected_file")
+        yield self.CSVTree("./data")
